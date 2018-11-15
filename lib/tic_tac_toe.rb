412a44e4351705b3085a57889a48a41712bdd845
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-15 03:17:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,16 @@
 class TicTacToe 
+  
+  WIN_COMBINATIONS = [
+    [0,1,2], # row wins
+    [3,4,5],
+    [6,7,8],
+    [0,3,6], # column wins
+    [1,4,7],
+    [2,5,8],
+    [0,4,8], # diagonal wins
+    [2,4,6]
+  ]
+
   def intitialize(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
     @board = board
   end
@@ -10,4 +22,8 @@ class TicTacToe
   def board
     @board
   end 
+ 
+ 
+  
+  
 end 
